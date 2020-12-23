29735b89c965806291c569d8ea9f149d020e106e
Aungthuhein187 <aungthuhein187@gmail.com>
2020-12-23 14:48:47 +0630

    enter

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -58,6 +58,7 @@ class Movies extends Component {
       selectedGenre,
       sortColumn,
     } = this.state;
+
     const filtered =
       selectedGenre && selectedGenre._id
         ? allMovies.filter((m) => m.genre._id === selectedGenre._id)
