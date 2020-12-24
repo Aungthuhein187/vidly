0ce0dd6506c8d6ffc1435d6e339d38e26766d951
Aungthuhein187 <aungthuhein187@gmail.com>
2020-12-24 15:06:52 +0630

    enter

--- src/services/fakeGenreService.js
+++
@@ -1,9 +0,0 @@
-export const genres = [
-  { _id: '6b21ca3eeb7f6fbccd471818', name: 'Action' },
-  { _id: '6b21ca3eeb7f6fbccd471819', name: 'Adventure' },
-  { _id: '6b21ca3eeb7f6fbccd471820', name: 'Crime' },
-];
-
-export function getGenres() {
-  return genres.filter((g) => g);
-}
