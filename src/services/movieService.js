40d228ca5f0875cb5729c755d35d6b0071a2de88
Aungthuhein187 <aungthuhein187@gmail.com>
2020-12-24 15:05:13 +0630

    refractor

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -3,7 +3,7 @@ import { apiUrl } from '../config.json';
 
 const apiEndpoint = `${apiUrl}/movies`;
 
-function getApiUrl(movieId) {
+function getMovieUrl(movieId) {
   return `${apiEndpoint}/${movieId}`;
 }
 
@@ -12,18 +12,18 @@ export function getMovies() {
 }
 
 export function getMovie(movieId) {
-  return http.get(this.getApiUrl(movieId));
+  return http.get(getMovieUrl(movieId));
 }
 
 export function saveMovie(movie) {
   if (movie._id) {
     const body = { ...movie };
     delete body._id;
-    return http.put(this.getApiUrl(movie._id), movie);
+    return http.put(getMovieUrl(movie._id), movie);
   }
   return http.post(apiEndpoint, movie);
 }
 
 export function deleteMovie(movieId) {
-  return http.delete(this.getApiUrl(movieId));
+  return http.delete(getMovieUrl(movieId));
 }
