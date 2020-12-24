b55735166918120c80197a1b52082d879669d05a
Aungthuhein187 <aungthuhein187@gmail.com>
2020-12-24 15:06:01 +0630

    enter

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -3,7 +3,7 @@ import { apiUrl } from '../config.json';
 
 const apiEndpoint = `${apiUrl}/movies`;
 
-function getMovieUrl(movieId) {
+function movieUrl(movieId) {
   return `${apiEndpoint}/${movieId}`;
 }
 
@@ -12,18 +12,18 @@ export function getMovies() {
 }
 
 export function getMovie(movieId) {
-  return http.get(getMovieUrl(movieId));
+  return http.get(movieUrl(movieId));
 }
 
 export function saveMovie(movie) {
   if (movie._id) {
     const body = { ...movie };
     delete body._id;
-    return http.put(getMovieUrl(movie._id), movie);
+    return http.put(movieUrl(movie._id), movie);
   }
   return http.post(apiEndpoint, movie);
 }
 
 export function deleteMovie(movieId) {
-  return http.delete(getMovieUrl(movieId));
+  return http.delete(movieUrl(movieId));
 }
