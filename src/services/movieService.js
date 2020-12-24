9e0da3556db7e0793f3473d41e5782f5e6890519
Aungthuhein187 <aungthuhein187@gmail.com>
2020-12-24 08:00:00 +0630

    enter

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -4,3 +4,9 @@ import { apiUrl } from '../config.json';
 export function getMovies() {
   return http.get(`${apiUrl}/movies`);
 }
+
+export function getMovie(id) {
+  return http.get(`${apiUrl}/movies/${id}`);
+}
+
+export function saveMovie(movie) {}
