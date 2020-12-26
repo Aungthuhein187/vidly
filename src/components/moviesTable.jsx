947d7aa4af9c7923b030d58630dfdd39e964cac2
Aungthuhein187 <aungthuhein187@gmail.com>
2020-12-26 18:05:44 +0630

    show delete button when user login & admin user

--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Like from './common/like';
 import Table from './common/table';
+import authService from '../services/authService';
 import { Link } from 'react-router-dom';
 
 class MoviesTable extends Component {
@@ -21,18 +22,27 @@ class MoviesTable extends Component {
         <Like like={movie.liked} onClick={() => this.props.onLike(movie)} />
       ),
     },
-    {
-      key: 'delete',
-      content: (movie) => (
-        <button
-          className="btn btn-danger btn-sm"
-          onClick={() => this.props.onDelete(movie)}
-        >
-          Delete
-        </button>
-      ),
-    },
   ];
+
+  deleteColumn = {
+    key: 'delete',
+    content: (movie) => (
+      <button
+        className="btn btn-danger btn-sm"
+        onClick={() => this.props.onDelete(movie)}
+      >
+        Delete
+      </button>
+    ),
+  };
+
+  constructor() {
+    super();
+    const user = authService.getCurrentUser();
+    if (user && user.isAdmin) {
+      this.columns.push(this.deleteColumn);
+    }
+  }
 
   render() {
     const { movies, onSort, sortColumn } = this.props;
