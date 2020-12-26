5833a4d432b87e5421396e96012cbbc66c38deff
Aungthuhein187 <aungthuhein187@gmail.com>
2020-12-26 08:00:49 +0630

    adding user name in navbar

--- src/App.js
+++ src/App.js
@@ -1,35 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Movies from './components/movies';
-import { Route, Switch, Redirect } from 'react-router-dom';
 import NavBar from './components/navBar';
 import Customers from './components/customers';
-import Rentals from './components/rentals';
-import NotFound from './components/not-found';
-import './App.css';
 import MovieForm from './components/movieForm';
 import LoginForm from './components/loginForm';
 import RegisterForm from './components/registerForm';
+import Rentals from './components/rentals';
+import NotFound from './components/not-found';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import jwt_decode from 'jwt-decode';
+import './App.css';
 
-function App() {
-  return (
-    <React.Fragment>
-      <NavBar />
-      <main className="container">
-        <Switch>
-          <Route path="/movies/:id" component={MovieForm} />
-          <Route path="/movies" component={Movies} />
-          <Route path="/movies/new" component={MovieForm} />
-          <Route path="/register" component={RegisterForm} />
-          <Route path="/customers" component={Customers} />
-          <Route path="/rentals" component={Rentals} />
-          <Route path="/login" component={LoginForm} />
-          <Route path="/not-found" component={NotFound} />
-          <Redirect from="/" exact to="/movies" />
-          <Redirect to="/not-found" />
-        </Switch>
-      </main>
-    </React.Fragment>
-  );
+class App extends Component {
+  state = {};
+
+  componentDidMount() {
+    try {
+      const jwt = localStorage.getItem('token');
+      const user = jwt_decode(jwt);
+      this.setState({ user });
+    } catch (error) {}
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar user={this.state.user} />
+        <main className="container">
+          <Switch>
+            <Route path="/movies/:id" component={MovieForm} />
+            <Route path="/movies" component={Movies} />
+            <Route path="/movies/new" component={MovieForm} />
+            <Route path="/register" component={RegisterForm} />
+            <Route path="/customers" component={Customers} />
+            <Route path="/rentals" component={Rentals} />
+            <Route path="/login" component={LoginForm} />
+            <Route path="/not-found" component={NotFound} />
+            <Redirect from="/" exact to="/movies" />
+            <Redirect to="/not-found" />
+          </Switch>
+        </main>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
